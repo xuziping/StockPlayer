2e500117c9b1093a5147767aaced6ad0aa9c9414
徐子平 <kxu@xuzipingdeMac-mini.local>
2017-11-30 00:23:14 +0800

    update

--- src/test/java/TestStock.java
+++ src/test/java/TestStock.java
@@ -1,6 +1,9 @@
 import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
@@ -8,6 +11,7 @@ import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -22,30 +26,53 @@ public class TestStock {
 //    private static final String URL = "https://link.zhihu.com/?target=http%3A//vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData%3Fpage%3D1%26num%3D100%26sort%3Dsymbol%26asc%3D1%26node%3Dhs_a%26symbol%3D%26_s_r_a%3Dinit";
 
     private static final String URL = "http://hq.sinajs.cn/list=%s";
-    private static Set<String> STOCKS = null;
-//"s_sh000001"
+    private static Set<Category> CATEGORIES = null;
+
+    //"s_sh000001"
     static {
-        STOCKS = Sets.newHashSet( "sh600000", "sz000568");
+        CATEGORIES = Sets.newHashSet(new Category("银行", Lists.newArrayList("600036", "000001", "600000", "601398", "601288", "601818")),
+                new Category("医药", Lists.newArrayList("600276", "600196", "000963")),
+                new Category("汽车", Lists.newArrayList("600104", "002594", "601238")),
+                new Category("保险", Lists.newArrayList("601318", "601628")),
+                new Category("酒/食品", Lists.newArrayList("000568", "000858", "000895")),
+                new Category("AI", Lists.newArrayList("002230", "002415", "002405")),
+                new Category("家电", Lists.newArrayList("600690", "000333")),
+                new Category("房地产", Lists.newArrayList("000002", "600655")),
+                new Category("电子", Lists.<String>newArrayList("000725", "000977", "002497")),
+                new Category("物流", Lists.newArrayList("002352")),
+                new Category("证券", Lists.newArrayList("600030", "002670", "000776")),
+                new Category("其它", Lists.newArrayList("601888", "601668"))
+        );
     }
 
     public static void main(String[] args) {
-        for (String stock : STOCKS) {
-            query(stock);
+        for (Category category : CATEGORIES) {
+            log.info("=============== {} =============", category.getName());
+            for (String stock : category.getStockCodeList()) {
+                query(stock);
+            }
         }
     }
 
     private static void query(String stockCode) {
         URL ur = null;
         try {
+            if (stockCode.startsWith("60")) {
+                stockCode = "sh" + stockCode;
+            } else {
+                stockCode = "sz" + stockCode;
+            }
             ur = new URL(String.format(URL, stockCode));
             HttpURLConnection uc = (HttpURLConnection) ur.openConnection();
             BufferedReader reader = new BufferedReader(new InputStreamReader(ur.openStream(), "GBK"));
             String line;
             while ((line = reader.readLine()) != null) {
                 Stock stock = parse(line);
+                stock.setCode(stockCode);
                 log.info(JSON.toJSONString(stock));
             }
         } catch (Exception e) {
+            log.error("Has error, stockCode={}", stockCode);
             e.printStackTrace();
         }
     }
@@ -62,6 +89,14 @@ public class TestStock {
         private BigDecimal current;
         private BigDecimal number;
         private BigDecimal amount;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class Category {
+        private String name;
+        private List<String> stockCodeList;
     }
 
     private static Stock parse(String val) throws Exception {
